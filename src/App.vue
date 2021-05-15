5d8b424a2b70a374ffa95b31e45cc8199c21e826
Paulo Vieira <vieiraweb.tecnologia@gmail.com>
2021-05-15 14:43:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-toolbar />
+    <v-content>
+      <app-carrousel />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+import Carrousel from "./Carrousel.vue";
+import Toolbar from "./Toolbar.vue";
+import Menu from "./Menu.vue";
+
+export default {
+  components: {
+    "app-menu": Menu,
+    "app-carrousel": Carrousel,
+    "app-toolbar": Toolbar,
+    "app-footer": Footer,
+  },
+};
+</script>
